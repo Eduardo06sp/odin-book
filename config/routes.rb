9dd79ce64d3c182e501f7f29164bca163464f83d
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-10-14 23:40:10 -0700

    feat: generate Devise User

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'posts#index'
 
+  devise_for :users
+
   get 'posts/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
