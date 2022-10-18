982a77d22482550ffe45b80481e642471da3a3b2
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-10-17 22:29:50 -0700

    test: ensure user is redirected to sign in when visiting Post index view

--- test/integration/post_flows_test.rb
+++ test/integration/post_flows_test.rb
@@ -4,4 +4,14 @@ class PostFlowsTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test 'redirects to sign in page if not signed in' do
+    get '/posts/index'
+
+    assert_redirected_to new_user_session_path
+    follow_redirect!
+    assert_response :success
+
+    assert_select 'h2', 'Log in'
+  end
 end
