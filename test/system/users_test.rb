72f69fd0b7b16bcff28d210c7e492e26ace2e63b
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-10-19 22:03:40 -0700

    test: ensure User can register

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -6,4 +6,17 @@ class UsersTest < ApplicationSystemTestCase
   #
   #   assert_selector "h1", text: "Users"
   # end
+
+  test 'signing up' do
+    visit new_user_registration_path
+    assert_selector 'h2', text: 'Sign up'
+
+    fill_in 'Email', with: 'sam@hunter.com'
+    fill_in 'Password', with: '123456'
+    fill_in 'Password confirmation', with: '123456'
+
+    click_on 'Sign up'
+
+    assert_text 'Posts#index'
+  end
 end
