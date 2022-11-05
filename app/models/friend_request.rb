8711f087ca3ea5c9a02fd0e73c70bcb78b29cc2e
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-11-04 20:45:02 -0700

    feat: add associations between users and friend requests

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -1,4 +1,4 @@
 class FriendRequest < ApplicationRecord
-  belongs_to :friend
+  belongs_to :friend, class_name: 'User'
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,5 @@ class User < ApplicationRecord
                           foreign_key: 'user_id',
                           association_foreign_key: 'friend_id',
                           join_table: 'friends_users'
+  has_many :friend_requests
 end
