4df4339771d95125d3c6cf0c609f46a4a5992ba1
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-11-07 23:01:28 -0800

    feat: generate UsersController

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  devise_scope :user do
+    get '/users/', to: 'users#index'
+    get '/users/:id', to: 'users#show'
+  end
+
   get 'posts/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
