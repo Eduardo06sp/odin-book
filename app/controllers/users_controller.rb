286a3b0a9b94c117a8bbf21965b178e5da83716e
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-11-27 20:20:56 -0800

    feat: allow user to cancel sent friend requests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
     @users = User.all
     @friend_requester_ids = friend_requests.pluck(:friend_id)
     @friend_ids = current_user.friend_ids
+    @friend_request_recipient_ids = FriendRequest.all.where(friend_id: current_user.id).pluck(:user_id)
   end
 
   def show
