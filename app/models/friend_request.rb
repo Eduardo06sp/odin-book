d8301d32c966038803bd0e9fc0581074fdecf6a4
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-11-30 23:35:19 -0800

    feat: prevent multiple friend requests for users with server-level validation

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -1,4 +1,13 @@
 class FriendRequest < ApplicationRecord
   belongs_to :friend, class_name: 'User'
   belongs_to :user
+
+  validates_uniqueness_of :user, scope: :friend
+  validate :cannot_have_received_request
+
+  def cannot_have_received_request
+    incoming_request = FriendRequest.where(user_id: friend_id, friend_id: user_id)
+
+    errors.add(:user, "can't have a pending friend request") if incoming_request.any?
+  end
 end
