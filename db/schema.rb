c36681712438463fbe2afbfb084c26fdce7a044d
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-12-15 19:54:27 -0800

    feat: generate Friendship through table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_30_075337) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_16_034754) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_075337) do
     t.index "LEAST(user_id, friend_id), GREATEST(user_id, friend_id)", name: "index_friend_requests_on_user_id_and_friend_id", unique: true
     t.index ["friend_id"], name: "index_friend_requests_on_friend_id"
     t.index ["user_id"], name: "index_friend_requests_on_user_id"
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "friend_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -47,4 +56,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_075337) do
 
   add_foreign_key "friend_requests", "users"
   add_foreign_key "friend_requests", "users", column: "friend_id"
+  add_foreign_key "friendships", "users"
+  add_foreign_key "friendships", "users", column: "friend_id"
 end
