4c6c7d0b9744c2be110555acc94e2f6131f802a2
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-12-15 20:26:20 -0800

    refactor: use through association instead of HABTM for Friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_and_belongs_to_many :friends,
-                          class_name: 'User',
-                          foreign_key: 'user_id',
-                          association_foreign_key: 'friend_id',
-                          join_table: 'friends_users'
   has_many :friend_requests
+  has_many :friendships
+  has_many :friends,
+           through: :friendships
 end
