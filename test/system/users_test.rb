1efc5314c899ad9472bd3100f31a5d167dad322b
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-12-17 10:37:09 -0800

    test: change non-unique email used to register in system test

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -11,7 +11,7 @@ class UsersTest < ApplicationSystemTestCase
     visit new_user_registration_path
     assert_selector 'h2', text: 'Sign up'
 
-    fill_in 'Email', with: 'sam@hunter.com'
+    fill_in 'Email', with: 'metatron@angel.org'
     fill_in 'Password', with: '123456'
     fill_in 'Password confirmation', with: '123456'
 
