a3f78ac29b37e542c8bcac4d9474dc60a3194732
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-12-17 10:27:58 -0800

    test: ensure duplicate friend requests cannot be sent

--- test/integration/friend_request_flow_test.rb
+++ test/integration/friend_request_flow_test.rb
@@ -23,4 +23,20 @@ class FriendRequestFlowTest < ActionDispatch::IntegrationTest
       assert_select 'button', text: 'Cancel Friend Request', count: 1
     end
   end
+
+  test 'cannot send duplicate friend requests' do
+    sign_in users(:dean)
+
+    post friend_requests_path,
+         params: { user: users(:sam).id }
+    assert_response :redirect
+
+    assert_no_difference('FriendRequest.count') do
+      post friend_requests_path,
+           params: { user: users(:sam).id }
+    end
+    assert_response :redirect
+
+    assert_equal 'Unable to send friend request.', flash[:alert]
+  end
 end
