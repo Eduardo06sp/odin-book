587f1ff93c0ddc04a72259c42ed02b2d62344191
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-12-17 14:27:34 -0800

    test: ensure user can add a friend

--- test/integration/user_flows_test.rb
+++ test/integration/user_flows_test.rb
@@ -6,4 +6,22 @@ class UserFlowsTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test 'can add friend' do
+    friend_counts = %w[users(:dean).friends.count users(:sam).friends.count]
+    sign_in users(:dean)
+
+    post friend_requests_path,
+         params: { user: users(:sam).id }
+    assert_response :redirect
+
+    assert_difference(friend_counts) do
+      post friendships_path,
+           params: {
+             user: users(:sam).id,
+             friend: users(:dean).id
+           }
+    end
+    assert_response :redirect
+  end
 end
