907502421b47733e326503cd0a3770edf79466d6
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-12-17 10:00:36 -0800

    test: ensure friend request can be sent

--- test/integration/friend_request_flow_test.rb
+++ test/integration/friend_request_flow_test.rb
@@ -6,4 +6,21 @@ class FriendRequestFlowTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test 'can send a friend request' do
+    sign_in users(:dean)
+
+    assert_difference('FriendRequest.count') do
+      post friend_requests_path,
+           params: { user: users(:sam).id }
+    end
+
+    assert_response :redirect
+
+    get users_path
+    assert_response :success
+    assert_select 'ul' do
+      assert_select 'button', text: 'Cancel Friend Request', count: 1
+    end
+  end
 end
