ac1bfc4d5b0151d51b05ac41932903a28acdf798
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-12-19 00:08:47 -0800

    feat: prevent duplicate friendships with db-level constraint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_16_034754) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_19_080312) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_16_034754) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["user_id", "friend_id"], name: "index_friendships_on_user_id_and_friend_id", unique: true
     t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
