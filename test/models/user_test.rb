296d95e874fc73cdb411ce3dd936fc52c4264d2e
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-12-26 21:08:50 -0800

    test: ensure duplicate friendship cannot be saved

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,15 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test 'should raise error when saving duplicate friendship' do
+    dean = users(:dean)
+    sam = users(:sam)
+
+    Friendship.create(user_id: sam.id, friend_id: dean.id)
+
+    assert_raises(ActiveRecord::RecordNotUnique) do
+      Friendship.create(user_id: sam.id, friend_id: dean.id)
+    end
+  end
 end
