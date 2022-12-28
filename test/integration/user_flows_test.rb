8f759fd2aa1217756e00432efa31c34688de897c
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-12-27 22:24:25 -0800

    test: ensure users are friends of eachother when accepting request

--- test/integration/user_flows_test.rb
+++ test/integration/user_flows_test.rb
@@ -25,6 +25,27 @@ class UserFlowsTest < ActionDispatch::IntegrationTest
     assert_response :redirect
   end
 
+  test 'user is friend of new friend after accepting friend request' do
+    friend_counts = %w[users(:dean).friends.count users(:sam).friends.count]
+    sign_in users(:dean)
+
+    post friend_requests_path,
+         params: { user: users(:sam).id }
+    assert_response :redirect
+
+    assert_difference(friend_counts) do
+      post friendships_path,
+           params: {
+             user: users(:sam).id,
+             friend: users(:dean).id
+           }
+    end
+    assert_response :redirect
+
+    assert_equal users(:sam), users(:dean).friends.first
+    assert_equal users(:dean), users(:sam).friends.first
+  end
+
   test 'cannot add friend without friend request' do
     sign_in users(:dean)
 
