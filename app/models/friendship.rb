91a6a6ebe5c1cba314d55942e76f462ae949c383
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-12-28 16:21:29 -0800

    feat: validate friend request presence in context of accepting request for Friendship

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,4 +2,18 @@ class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend,
              class_name: 'User'
+
+  validate :friend_request_present?, on: :accept_friend_request
+
+  def friend_request_present?
+    friend_request = FriendRequest.find_by(user_id: user, friend_id: friend)
+
+    if friend_request.present?
+      puts 'FRIEND REQUEST PRESENT; PASS'
+    else
+      errors.add :base,
+                 :friend_request_missing,
+                 message: 'associated friend request missing'
+    end
+  end
 end
