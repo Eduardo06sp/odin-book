181f840c4e4e5b8a14c0361d0b06faad4f769153
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-12-30 21:24:08 -0800

    refactor!: add friend back using filter instead of in FriendshipsController#create
    BREAKING CHANGE: after_action will always run since no exceptions are raised, but refactoring is in progress

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,13 +1,14 @@
 class FriendshipsController < ApplicationController
+  after_action :add_friend_back, only: :create
+
   def create
     user = User.find_by(id: params[:user])
     friend = User.find_by(id: params[:friend])
     friend_request = FriendRequest.find_by(friend_id: friend, user_id: user)
 
     friendship = user.friendships.build(friend_id: friend.id)
-    inverse_friendship = friend.friendships.build(friend_id: user.id)
 
-    if friendship.save(context: :accept_friend_request) && inverse_friendship.save
+    if friendship.save(context: :accept_friend_request)
       friend_request.destroy
 
       puts 'SAY HELLO, YOU ARE NOW FRENDS'
@@ -31,5 +32,16 @@ class FriendshipsController < ApplicationController
     end
 
     redirect_back_or_to root_path
+  end
+
+  def add_friend_back
+    friend = User.find_by(id: params[:friend])
+    inverse_friendship = friend.friendships.build(friend_id: params[:user])
+
+    if inverse_friendship.save
+      puts 'SAY HELLO BACK, YA GOT ADDED BACK AUTOMATICALLY'
+    else
+      flash[:alert] = 'Unable to complete friendship.'
+    end
   end
 end
