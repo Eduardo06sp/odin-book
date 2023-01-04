b2e78d7cc8a51b82f193ffe774443a64bec492be
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2023-01-04 15:46:41 -0800

    refactor: use resourceful routes for Users

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   devise_scope :user do
-    get '/users/', to: 'users#index'
-    get '/users/:id', to: 'users#show'
+    resources :users, only: %i[index show]
   end
 
   get 'posts/index'
