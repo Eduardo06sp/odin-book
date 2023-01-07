06774e53ae0cdb73140300148357b24372fd1b70
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2023-01-06 23:24:55 -0800

    feat: notify user after creating friend request

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -1,4 +1,6 @@
 class FriendRequestsController < ApplicationController
+  after_action :notify_user, only: :create
+
   def create
     user = User.find_by(id: params[:user])
     new_friend_request = user.friend_requests.build(friend_id: current_user.id)
@@ -20,5 +22,16 @@ class FriendRequestsController < ApplicationController
     friend_request.destroy
 
     redirect_back_or_to root_path
+  end
+
+  def notify_user
+    user = User.find_by(id: params[:user])
+    friend = User.find_by(id: params[:friend])
+
+    user.notifications.create!(description:
+                                 '<span class="notification_username">'\
+                                 "#{friend.email}"\
+                                 '</span>'\
+                                 ' sent you a friend request.')
   end
 end
