7ef494448e5c15bd361278a5e1f429f0ddf6e746
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2023-01-11 23:31:23 -0800

    test: generate IntegrationsFlowsTest

---
+++ test/integration/notification_flows_test.rb
@@ -0,0 +1,9 @@
+require "test_helper"
+
+class NotificationFlowsTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  # test "the truth" do
+  #   assert true
+  # end
+end
