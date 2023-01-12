20842f9fda8f2049e1fa6d210fd93a9aa9046745
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2023-01-11 23:55:45 -0800

    test: ensure notification is created after friend request

--- test/integration/notification_flows_test.rb
+++ test/integration/notification_flows_test.rb
@@ -6,4 +6,21 @@ class NotificationFlowsTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test 'should create notification after friend request' do
+    friend_notifications = users(:sam).notifications
+
+    sign_in users(:dean)
+
+    post friend_requests_path,
+         params: {
+           user: users(:sam).id,
+           friend: users(:dean).id
+         }
+
+    assert_response :redirect
+    assert_equal '<span class="notification_username">dean@hunter.com</span> '\
+                 'sent you a friend request.',
+                 friend_notifications.first.description
+  end
 end
