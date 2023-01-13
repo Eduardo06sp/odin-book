d197c13099d3d6186debabe84cdf2e8ae0681e17
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2023-01-12 19:03:33 -0800

    feat: add destroy action to NotificationsController

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,4 +2,16 @@ class NotificationsController < ApplicationController
   def index
     @notifications = current_user.notifications
   end
+
+  def destroy
+    notification = Notification.find(params[:id])
+
+    if notification.destroy
+      flash[:notice] = 'Notification removed.'
+    else
+      flash[:alert] = 'Unable to remove notification.'
+    end
+
+    redirect_back_or_to root_path
+  end
 end
