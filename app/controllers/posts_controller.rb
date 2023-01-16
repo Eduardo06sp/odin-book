5e9ce8d2d2783d0a2cb284b90003c047aba18c67
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2023-01-15 22:01:06 -0800

    feat: create new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,26 @@
 class PostsController < ApplicationController
   def index
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      flash[:notice] = 'Successfully created new post.'
+      redirect_to root_path
+    else
+      flash[:alert] = 'Unable to create post.'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
