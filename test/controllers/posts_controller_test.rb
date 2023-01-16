27da6a92629c2116e15e62c0654a401005e04a2d
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2023-01-15 22:02:47 -0800

    test: add Devise integration helpers to PostsControllerTest

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,6 +1,8 @@
 require "test_helper"
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   # test "should get index" do
   #   get posts_index_url
   #   assert_response :success
