b07736f9d1d51db4f675f8273bb1756d947298f5
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2023-01-20 21:13:50 -0800

    feat: allow user to like posts

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,16 @@
 class LikesController < ApplicationController
   def create
+    post = Post.find_by(id: params[:post])
+    user = User.find_by(id: params[:user])
+
+    like = Like.new(post:, user:)
+
+    if like.save
+      flash[:notice] = 'Liked the post!'
+    else
+      flash[:alert] = 'Unable to like post.'
+    end
+
+    redirect_to root_path
   end
 end
