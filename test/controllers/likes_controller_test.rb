b072f54432d6c6f2232aca25bbabc63c25a89b7f
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2023-01-20 21:30:17 -0800

    test: ensure like is created

--- test/controllers/likes_controller_test.rb
+++ test/controllers/likes_controller_test.rb
@@ -1,8 +1,25 @@
 require "test_helper"
 
 class LikesControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   # test "should get create" do
   #   get likes_create_url
   #   assert_response :success
   # end
+
+  test 'can create a like' do
+    sign_in users(:dean)
+
+    assert_difference('Like.count') do
+      post likes_path,
+           params: {
+             user: users(:dean).id,
+             post: posts(:dean_post).id
+           }
+    end
+
+    assert_response :redirect
+    assert_equal 'Liked the post!', flash[:notice]
+  end
 end
