9278a450b62449a5aa6d69e7ecb25775d7737b31
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2023-01-25 23:09:33 -0800

    feat: add create action route for comments

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,9 @@ Rails.application.routes.draw do
   resources :friendships, only: %i[create destroy]
 
   resources :notifications, only: %i[index destroy]
+
+  resources :comments, only: %i[create]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
