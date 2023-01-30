b772768884448a640c7109b6e62b1f284c9ae419
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2023-01-29 20:35:11 -0800

    feat: add ability to submit comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,19 @@
 class CommentsController < ApplicationController
   def create
+    comment = current_user.comments.build(comment_params)
+
+    if comment.save
+      flash[:notice] = 'Added comment.'
+    else
+      flash[:alert] = 'Unable to add comment.'
+    end
+
+    redirect_back_or_to root_path
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :post_id)
   end
 end
