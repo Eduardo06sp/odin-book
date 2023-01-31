cf8668d67ec1f83140ac98d32dc494431cc48b3d
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2023-01-30 19:34:28 -0800

    test: ensure comment is created

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -7,4 +7,20 @@ class CommentsControllerTest < ActionDispatch::IntegrationTest
   #   get comments_create_url
   #   assert_response :success
   # end
+
+  test 'should create comment' do
+    sign_in users(:dean)
+
+    assert_difference('Comment.all.length') do
+      post comments_path, params: {
+        comment: {
+          content: 'greetings fellas',
+          post_id: posts(:dean_post).id},
+        commit: 'Create Comment'
+      }
+    end
+
+    assert_response :redirect
+    assert_equal 'Added comment.', flash[:notice]
+  end
 end
