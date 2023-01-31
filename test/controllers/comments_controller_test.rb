25f47cf33d490f6a25f12d5026f4e7a115ee0093
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2023-01-30 19:33:02 -0800

    test: include Devise test helpers

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,6 +1,8 @@
 require "test_helper"
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   # test "should get create" do
   #   get comments_create_url
   #   assert_response :success
