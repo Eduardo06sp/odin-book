5ed17d0556d430fab49eac61f1264eb4be4d8797
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2023-04-24 22:20:00 -0700

    feat: add omniauth-related columns to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_31_060722) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_25_051934) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -113,6 +113,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_31_060722) do
     t.text "state"
     t.text "city"
     t.text "real_name"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
