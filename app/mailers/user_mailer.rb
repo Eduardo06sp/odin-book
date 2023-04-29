ce7807f33d22cdd5fee284396672ee8c782c364e
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2023-04-28 21:48:27 -0700

    feat: add method for welcome email in UserMailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,8 @@
 class UserMailer < ApplicationMailer
+  default from: 'welcome@odinbook.com'
+
+  def welcome_email
+    @user = params[:user]
+    mail(to: @user.email, subject: 'Welcome to Odin-book!')
+  end
 end
