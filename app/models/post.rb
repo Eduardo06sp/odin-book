1ba402ead8658f4d570b557bbdbbe4540bf96e9f
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2023-05-13 14:15:13 -0700

    feat: validate size and format of images attached to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,5 +13,21 @@ class Post < ApplicationRecord
 
   has_one_attached :image
 
+  validate :image_format, :image_size, if: -> { image.attached? }
+
   scope :user_and_friends, ->(user) { where(user_id: [[user.id] + user.friend_ids]) }
+
+  private
+
+  def image_format
+    unless image.blob.image?
+      errors.add(:base, 'Attached file must be an image')
+    end
+  end
+
+  def image_size
+    if image.blob.byte_size > 5.megabytes
+      errors.add(:base, 'Attached file size must be less than 5 megabytes')
+    end
+  end
 end
