2ddc2d58798f4dedbde95c285f168c4d4d342a8c
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2023-06-10 22:37:12 -0700

    test: ensure new post gets created

--- test/integration/post_flows_test.rb
+++ test/integration/post_flows_test.rb
@@ -1,6 +1,8 @@
 require "test_helper"
 
 class PostFlowsTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   # test "the truth" do
   #   assert true
   # end
@@ -13,5 +15,21 @@ class PostFlowsTest < ActionDispatch::IntegrationTest
     assert_response :success
 
     assert_select 'h2', 'Log in'
+  end
+
+  test 'create new text post' do
+    sign_in users(:dean)
+
+    post posts_path,
+      params: {
+        post: {
+          content: 'I am out of salt!'
+        }
+      }
+
+    assert_response :redirect
+    follow_redirect!
+
+    assert_select 'li', /I am out of salt!/
   end
 end
