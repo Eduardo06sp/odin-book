487ab8b0f0a21a674927e0140254db5a07196681
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2023-06-20 01:48:27 -0700

    feat: clear attachment inputs when modal is cancelled

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,10 +1,20 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ['modal']
+  static targets = ['modal', 'uploadUrl', 'uploadFile']
 
   display () {
     this.modalTarget.showModal()
+  }
+
+  clearValue (element) {
+    element.value = ''
+  }
+
+  cancel () {
+    this.clearValue(this.uploadUrlTarget)
+    this.clearValue(this.uploadFileTarget)
+    this.close()
   }
 
   close () {
