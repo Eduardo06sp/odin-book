539ade017f5a308d82ffc2356a1d0882527543a0
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2023-07-26 23:43:53 -0700

    test: ensure user can create new comment on another post

--- test/integration/comment_flows_test.rb
+++ test/integration/comment_flows_test.rb
@@ -1,8 +1,23 @@
 require "test_helper"
 
 class CommentFlowsTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
   include Devise::Test::IntegrationHelpers
+
+  test 'create new comment' do
+    sign_in users(:castiel)
+
+    post comments_path,
+      params: {
+        comment: {
+          content: "I'm right here.",
+          post_id: posts(:sam_post).id
+        }
+      }
+
+    assert_response :redirect
+    follow_redirect!
+
+    assert_select '.comments-toggle', '1 Comment'
+    assert_equal "I'm right here.", posts(:sam_post).comments.first.content
+  end
 end
