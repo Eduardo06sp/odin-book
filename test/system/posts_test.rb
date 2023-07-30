d59316a866d2bd2a81be0489103218bcfceae8a0
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2023-07-30 00:08:28 -0700

    test: ensure post is created

--- test/system/posts_test.rb
+++ test/system/posts_test.rb
@@ -2,4 +2,17 @@ require "application_system_test_case"
 
 class PostsTest < ApplicationSystemTestCase
   include Devise::Test::IntegrationHelpers
+
+  test 'creating text-only post' do
+    sign_in users(:dean)
+
+    visit posts_path
+    assert_selector 'h2', text: 'Your feed'
+
+    fill_in 'post_content', with: 'I am looking for Cas!'
+
+    click_button 'Create Post'
+
+    assert_selector 'p.post-content', text: 'I am looking for Cas!'
+  end
 end
