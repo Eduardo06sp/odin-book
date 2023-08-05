7946150617711f27733f24f69f8a9cd98762097b
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2023-08-04 19:01:21 -0700

    feat: use turbo stream to add new comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,17 @@
 class CommentsController < ApplicationController
   def create
-    comment = current_user.comments.build(comment_params)
+    @comment = current_user.comments.build(comment_params)
 
-    if comment.save
+    if @comment.save
       flash[:notice] = 'Added comment.'
     else
       flash[:alert] = 'Unable to add comment.'
     end
 
-    redirect_back_or_to root_path
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_back_or_to root_path }
+    end
   end
 
   private
