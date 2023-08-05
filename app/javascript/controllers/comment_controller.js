1ea5d654061d61fb255fb51852a71f6150c1e060
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2023-08-05 14:30:32 -0700

    refactor: hide new comment form when stimulus controller connects

--- app/javascript/controllers/comment_controller.js
+++ app/javascript/controllers/comment_controller.js
@@ -3,6 +3,10 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ['newCommentForm', 'comments']
 
+  connect () {
+    this.newCommentFormTarget.classList.add('hidden')
+  }
+
   toggleVisibility () {
     this.newCommentFormTarget.classList.toggle('hidden')
     this.commentsTarget.classList.toggle('hidden')
